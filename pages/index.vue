a64d697ba14e2717836e3960dce331d5172b66ef
ecki-d <support@kaizenmedia.co.za>
2019-07-15 13:38:47 +0200

    Create Netlify config.

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,13 @@
     <div>
       <logo />
       <h1 class="title">
-        namdrive
+        Namdrive Academy
       </h1>
       <h2 class="subtitle">
         Apply online for driving lessons in Namibia.
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
         </a>
         <a
@@ -29,13 +25,13 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   components: {
     Logo
   }
-}
+};
 </script>
 
 <style>
@@ -49,8 +45,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
